d0cb8dd22a09e9f1db2146f8586b9085d9c40f6b
Gonza <peraltag@gmail.com>
2016-04-04 00:39:48 -0300

    Agrego rails app

--- app/app.js
+++ app/app.js
@@ -10,21 +10,25 @@ function($stateProvider, $urlRouterProvider) {
         templateUrl: '/home.html',
         controller: 'MainCtrl'
       });
+    $stateProvider
+      .state('posts', {
+        url: '/posts/{id}',
+        templateUrl: '/posts.html',
+        controller: 'PostsCtrl'
+      });  
 
     $urlRouterProvider.otherwise('home');
 }]);
 
-//angular.module('flapperNews')
 flappernews
 .factory('posts', [function(){
     var service = {
-        posts: [{title: 'Blaaa', upvotes:0}]
+        posts: []
     };
 
     return service;
 }]);
 
-//angular.module('flapperNews')
 flappernews
 .controller('MainCtrl', [
 '$scope',
@@ -34,17 +38,41 @@ function($scope, posts){
   $scope.posts = posts.posts;
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-        $scope.posts.push({
-            title: $scope.title,
-            link: $scope.link,
-            upvotes: 0
-        });
-        $scope.title = '';
-        $scope.link = '';
+    $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
+    });
+    $scope.title = '';
+    $scope.link = '';
   };
   $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
+      post.upvotes += 1;
   };
+  
 }
 ]);
 
+
+flappernews
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+  $scope.addComment = function(){
+    if($scope.body === '') { return; }
+    $scope.post.comments.push({
+      body: $scope.body,
+      author: 'user',
+      upvotes: 0
+    });
+    $scope.body = '';
+  };
+}
+]);
